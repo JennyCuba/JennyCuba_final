6a734aa489ecb192e9e63e60f01b9770da7d71f1
Jenny Cuba <jenny.cuba.c@gmail.com>
2025-02-10 18:01:55 -0300

    cambios

--- p_django/urls.py
+++ p_django/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import bienvenida, fecha_y_hora, mi_template, saludo, mi_template2
+from .views import bienvenida, fecha_y_hora, mi_template, saludo, mi_template2, condicionales_y_bucles
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +26,6 @@ urlpatterns = [
     path('fecha_y_hora/', fecha_y_hora),
     path('mi_template/', mi_template),
     path('mi_template2/', mi_template2),
+    path('condicionales_y_bucles/', condicionales_y_bucles),
 ]
 
--- p_django/views.py
+++ p_django/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from datetime import datetime
 from django.template import Template, Context, loader
+from django.shortcuts import render
 
 def bienvenida(request):
     return HttpResponse ('<h1>Bienvenida!<h1/>')
@@ -9,28 +10,45 @@ def fecha_y_hora(request):
     fecha_y_hora = datetime.now()
     return HttpResponse (f'<h1>esta vista muestra la fecha y hora<h1/>\n{fecha_y_hora}')
 
-def mi_template(request):
-    
-# V1
-    archivo_abierto = open('Templates\mi_template.html')
-    template = Template(archivo_abierto.read())
-    archivo_abierto.close()
-    
-    contexto = Context({'Nombre': 'Jenny'})
-    template_renderizado = template.render(contexto)
-
-    return HttpResponse(template_renderizado)
-
 def saludo(request, nombre, apellido):
     nombre_formateado = nombre.title()
     apellido_formateado = apellido.title()
     return HttpResponse(f"Buenas {nombre_formateado} {apellido_formateado}, como va?")
 
+def mi_template(request):
+    
+# V1
+    #archivo_abierto = open('Templates\mi_template.html')
+    #template = Template(archivo_abierto.read())
+    #archivo_abierto.close()
+    
+    #contexto = Context({'Nombre': 'Jenny'})
+    #template_renderizado = template.render(contexto)
+
+    #return HttpResponse(template_renderizado)
+    
+# v2 con cargador y shortcuts
+    template = loader.get_template('mi_template.html')
+    return render(request, 'mi_template.html', {'Nombre': 'Jenny'})
+
+
 def mi_template2(request):
     
-# ejemplo template con cargador
-    template = loader.get_template('mi_template2.html')
-    diccionario = {'Nombre': 'Jenny'}
-    template_renderizado = template.render(diccionario)
+# v1 ejemplo template con cargador
+    #template = loader.get_template('mi_template2.html')
+    #diccionario = {'Nombre': 'Jenny'}
+    #template_renderizado = template.render(diccionario)
 
-    return HttpResponse(template_renderizado)
+    #return HttpResponse(template_renderizado)
+# v2 resumida
+    template = loader.get_template('mi_template2.html')
+    return render(request, 'mi_template2.html')
+
+def condicionales_y_bucles(request):
+    
+   
+    return render(request, 'condicionales_y_bucles.html',  {
+        'Nombre': 'Jenny',
+        'Mis_elementos': [],
+        'Numeros': list(range(15))
+    })
