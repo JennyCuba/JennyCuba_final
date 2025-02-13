f468c95d942561b5d2ab0a7ac02a00e7d89e93e3
Jenny Cuba <jenny.cuba.c@gmail.com>
2025-02-13 17:38:00 -0300

    primer modelo

--- inicio/models.py
+++ inicio/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class auto(models.Model):
+    marca = models.CharField(max_length=20)
+    modelo = models.CharField(max_length=20)
+    anio = models.IntegerField()
